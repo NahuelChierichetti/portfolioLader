10a2216acced1d3616a2e089ea64bdf5c8cafb88
Nahuel Chierichetti <nahuelchierichetti@gmail.com>
2024-03-15 14:15:43 -0300

    Solución 404

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -56,9 +56,6 @@ const NavBar = () => {
               <li className="uppercase link-menu">
                 <Link to={'/categoria/RedesSociales'}>Redes Sociales</Link>
               </li>
-              <li className="uppercase link-menu">
-                <Link to={'/panel'}>Panel</Link>
-              </li>
             </ul>
           </div>
           <div className="hidden lg:flex">
